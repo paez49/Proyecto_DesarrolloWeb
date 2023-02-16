ce9aa7a5e700669b0c67de910bb5154c58e1a547
sleal <56454387+SLealOmana@users.noreply.github.com>
2023-02-16 16:33:43 +0000

    cosas bonitas

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,15 @@ function validarFormulario() {
       return false;
     }
 }
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+const image = document.querySelector('.div-izquierdo img');
+
+image.addEventListener('mouseover', () => {
+  image.parentElement.classList.add('hover');
+});
+
+image.addEventListener('mouseout', () => {
+  image.parentElement.classList.remove('hover');
+});
